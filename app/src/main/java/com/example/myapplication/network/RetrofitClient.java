763d9a6ba308f57cfa7acfd0b5209dd42ff12771
boaz1e <boazy@my.hit.ac.il>
2024-08-29 15:48:26 +0300

    refactor: Simplify the BASE URL

--- app/src/main/java/com/example/myapplication/network/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/network/RetrofitClient.java
@@ -16,6 +16,7 @@ public class RetrofitClient {
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
+
         return retrofit;
     }
 }
